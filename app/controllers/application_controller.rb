2a22eb12b89cac0640dac0973f3c9b70ea57c674
warrenbrown <warren@katapultmedia.com>
2016-03-03 09:05:33 -0600

    removed twitter info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :load_tweets
-
-  def load_tweets
-    @tweets = Twitter.user_timeline[0..4] # For this demonstration lets keep the tweets limited to the first 5 available.
-  end
+  
 end
