e367a2a733d133ed68eeea069561efb2d993c8fd
warrenbrown <warren@katapultmedia.com>
2016-03-07 20:03:23 -0600

    added paperclip to s3

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   validates :title, presence: true
   has_many :comments, dependent: :destroy
 
-  has_attached_file :article_img, styles: { large: "650x450" ,medium: "300x300>", thumb: "100 x 100>" }, default_url: "http://placehold.it/350x150"
+  has_attached_file :article_img, styles: { medium: "300x300>", thumb: "100 x 100>" }, default_url: "http://placehold.it/350x150"
   validates_attachment_content_type :article_img, content_type: /\Aimage\/.*\Z/
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,5 +42,12 @@ Rails.application.configure do
   # config/environments/production.rb
   # Sets Paperclip to upload images to AMazon s3
 
-  
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
